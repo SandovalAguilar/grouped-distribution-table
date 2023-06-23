09331bdb90e3a39b8a7020ca58d59c1f19c96cd0
SandovalAguilar <ismael.sandoval.aguilar@gmail.com>
2023-06-23 16:18:33 -0400

    Completed method that generates intervals

--- base_gdt/grouped_distribution_table/generate_table.py
+++ base_gdt/grouped_distribution_table/generate_table.py
@@ -11,6 +11,7 @@ class groupedTable:
         self.column_name = self.df.columns[0]
         self.number_classes = self.calculate_number_classes()
         self.amplitude = self.calculate_amplitude()
+        self.array_intervals = self.generate_intervals()
          
     def calculate_number_classes(self):
         self.number_classes = int(np.ceil(np.sqrt(self.length))) if self.length < 30 else int(np.ceil(1 + (3.322 * np.log10(self.length))))
@@ -21,7 +22,22 @@ class groupedTable:
         return self.amplitude 
     
     def generate_intervals(self):
-        pass
+        array_intervals = np.empty((self.number_classes, 2), dtype=float)
+        array_intervals[0][0] = self.df.min().iloc[0] 
+        array_intervals[0][1] = self.df.min().iloc[0] + self.amplitude.iloc[0]
+        aux_col1 = self.df.min().iloc[0]
+        aux_col2 = self.df.min().iloc[0] + self.amplitude.iloc[0]
+
+        for i in range(1,self.number_classes):
+            aux_col1 += self.amplitude
+            array_intervals[i][0] = aux_col1.iloc[0]
+                
+        for i in range(1,self.number_classes):
+            aux_col2 += self.amplitude
+            array_intervals[i][1] = aux_col2.iloc[0]
+
+        return array_intervals
+
 
     def calculate_freq(self):
         pass
--- base_gdt/test.py
+++ base_gdt/test.py
@@ -2,7 +2,7 @@ import grouped_distribution_table as gdt
 
 def main():
     grouped_table = gdt.groupedTable('Tabla', 'hombres.csv')
-    print(grouped_table.calculate_amplitude())
+    print(grouped_table.generate_intervals())
 
 if __name__ == '__main__':
     main()
