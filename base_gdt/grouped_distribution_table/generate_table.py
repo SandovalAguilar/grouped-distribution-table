8a3710a6cf9bf7f92307ab47216fc6f0146c53a9
SandovalAguilar <ismael.sandoval.aguilar@gmail.com>
2023-06-26 18:05:32 -0400

    Added freq column function

--- base_gdt/grouped_distribution_table/generate_table.py
+++ base_gdt/grouped_distribution_table/generate_table.py
@@ -12,6 +12,7 @@ class groupedTable:
         self.number_classes = self.calculate_number_classes()
         self.amplitude = self.calculate_amplitude()
         self.array_intervals = self.generate_intervals()
+        self.array_abs_freq = self.generate_abs_freq()
          
     def calculate_number_classes(self):
         self.number_classes = int(np.ceil(np.sqrt(self.length))) if self.length < 30 else int(np.ceil(1 + (3.322 * np.log10(self.length))))
@@ -35,9 +36,25 @@ class groupedTable:
         for i in range(1,self.number_classes):
             aux_col2 += self.amplitude
             array_intervals[i][1] = aux_col2.iloc[0]
+        
+        '''
+        array_intervals = [array_intervals[i][0] = (aux_col1 := aux_col1 + amplitud).iloc[0] for i in range(1, self.number_classes)]
+        array_intervals = [array_intervals[i][1] = (aux_col2 := aux_col2 + amplitud).iloc[0] for i in range(1, self.number_classes)]
+        '''    
 
-        return array_intervals
+        return self.array_intervals
 
+    def count_range_in_list(array, interval):
+        for i in array:
+            if i != array[-1]:
+                if interval[0] <= i < interval[1]:
+                    counts += 1
+            else:
+                if interval[0] <= i <= interval[1]:
+                    counts += 1
+        
+        return counts
 
-    def calculate_freq(self):
-        pass
+    def generate_abs_freq(self):
+        for i in range(self.number_classes):
+	        self.array_abs_freq[i] = self.count_range_in_list(self.df[self.df.columns[0]].values, self.array_classes[i])
